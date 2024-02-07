3e7ee1f1044ed7d98ee6b51dbd682ec4eea5cc6d
parthpatel-01 <parth.patel45610@gmail.com>
2024-02-07 00:34:22 -0600

    This is a first commit.

---
+++ Tasks/View/UserControls/AddNewTask.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tasks.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for AddNewTask.xaml
+    /// </summary>
+    public partial class AddNewTask : UserControl
+    {
+        public AddNewTask()
+        {
+            InitializeComponent();
+        }
+
+        private void NewTaskBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(NewTaskBox.Text != "")
+            {
+                NewTaskBlk.Visibility = Visibility.Hidden;
+            } else if (NewTaskBox.Text == "")
+            {
+                NewTaskBlk.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void AddTaskBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NewTaskBox.Text = string.Empty;
+        }
+    }
+}
